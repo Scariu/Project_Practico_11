46bc8f6ec52d44f36e8276386200509239f69052
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-13 20:05:28 -0400

    Se realiza ejercicio A.

--- app/src/main/java/com/example/project_practico_11/MainActivity.kt
+++ app/src/main/java/com/example/project_practico_11/MainActivity.kt
@@ -9,3 +9,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+//Ejercicio A
+fun main() {
+    var num1 = 10
+    val num2 = 33
+    val num3 = 66
+    var resultado = num1 + num2 + num3
+    println(resultado)
+
+    num1 = 55
+    resultado = num1 + num2 + num3
+    println(resultado)
+
+    val promedio = resultado/3
+
+    println("El promedio es $promedio")
+}
+
+
